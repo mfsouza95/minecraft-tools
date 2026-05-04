f51101b8e31c04b7cd76354af2666489780a9482
mfsouza95 <muriloferraz2002@gmail.com>
2026-05-04 12:08:18 -0300

    new title

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="absolute inset-0 bg-black opacity-30"></div>
       <div className="relative z-10 py-4">
         <div className="border-4 rounded-sm p-4 flex justify-center w-fit mx-auto drop-shadow-lg backdrop-blur-xs bg-white/10">
-          <h1 className="font-[family-name:var(--font-minecraft)] text-4xl">EASY MINE</h1>
+          <h1 className="font-[family-name:var(--font-minecraft)] text-4xl">CREEPER TOOLS</h1>
         </div>
         <div className="border-4 rounded-lg w-fit mx-auto justify-center p-4 m-8 grid grid-cols-3 grid-rows-6 drop-shadow-2xl backdrop-blur-xs bg-white/20 font-[family-name:var(--font-minecraft)]">
           <Link className="border-4 rounded-lg mx-4 p-2 text-center border-rose-900 bg-rose-900/30 font-semibold" href="/nether">Nether</Link>
