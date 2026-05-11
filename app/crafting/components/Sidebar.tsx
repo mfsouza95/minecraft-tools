ccb3018a08a5faf7604788a08024a15a1103378c
mfsouza95 <muriloferraz2002@gmail.com>
2026-05-11 13:13:19 -0300

    open and close button for sidebar - crafting tool

--- app/crafting/components/Sidebar.tsx
+++ app/crafting/components/Sidebar.tsx
@@ -9,25 +9,37 @@ interface SidebarProps {
     onClearHistory: () => void
 };
 
+let backButton: string = ''
+
 export default function Sidebar({ history, onSelectEntry, onClearHistory }: SidebarProps){
     const [sidebarState, setSidebarState] = useState(true);
+
+    if (!sidebarState){
+        backButton = '>'
+    } else {
+        backButton = '<'
+    }
 
     const toggleSidebar = () => setSidebarState(!sidebarState);
     
     useEffect(() => {
         if(history.length > 0){
             setSidebarState(false)
-            console.log(history);
         }
     }, [history]);
 
     return(
         <div>
             <aside className= {`fixed right-0 top-0 z-20 rounded-l-lg flex flex-col bg-white text-black transition-all duration-300 ease-in-out ${sidebarState ? 'w-20' : 'w-92'} h-screen p-4`}>
-                <h1 className='text-center font-bold text-3xl pb-4'>History:</h1>
+                <div className='grid grid-cols-6 pb-4'>
+                    <input type="button" value={backButton} className='absolute size-10 text-3xl font-bold border-2 rounded-lg cursor-pointer transition-all hover:scale-105 hover:shadow-md hover:shadow-black/20 hover:bg-gray-200'
+                        onClick={toggleSidebar}
+                    />
+                    <h1 className={`text-center font-bold text-3xl pb-4 col-span-4 col-start-2 ${sidebarState? 'invisible' : 'visible' }`}>History:</h1>
+                </div>
                 <ul>
                     {history.map((item, index) => (
-                        <li className= 'cursor-pointer p-3 border-b border-gray-300 transition-all hover:scale-105 hover:shadow-md hover:shadow-black/20 hover:bg-gray-50' key={index} onClick={() => onSelectEntry(item)}>
+                        <li className= {`cursor-pointer p-3 border-b border-gray-300 transition-all hover:scale-105 hover:shadow-md hover:shadow-black/20 hover:bg-gray-50 ${sidebarState? 'invisible' : 'visible' }`} key={index} onClick={() => onSelectEntry(item)}>
                             <p className='font-bold'>{item.selectedItem}: {item.quantity}</p>
                             {Object.entries(item.rawMaterials).map(([name, data]) => (
                                 <p key={name} className='pl-4'>{name}: {data.quantity}</p>
@@ -35,7 +47,7 @@ export default function Sidebar({ history, onSelectEntry, onClearHistory }: Side
                         </li>
                     ))}
                 </ul>
-                <div className='mt-auto grid grid-cols-2 gap-2 h-12'>
+                <div className={`mt-auto grid grid-cols-2 gap-2 h-12 ${sidebarState? 'invisible' : 'visible' }`}>
                     <input type="button" value="Clear History" className='border-2 border-black cursor-pointer rounded-lg transition-all hover:scale-105 hover:shadow-md hover:shadow-black/20 hover:bg-gray-200'
                     onClick={onClearHistory}
                     />
